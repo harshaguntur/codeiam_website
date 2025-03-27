6cd4ce2a39a9ee0bcb324de381f43df1691d08de
YGN <ygntechstartup@gmail.com>
2025-03-27 11:12:33 +0530

    updated the webdev recruitment

--- app/WebDEVRECRUITMENT/page.tsx
+++ app/WebDEVRECRUITMENT/page.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Page = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen mt-40 bg-gradient-to-br from-blue-400 via-blue-300 to-blue-500 p-4">
-      <div className="backdrop-blur-lg bg-white/20 shadow-lg rounded-2xl p-6 w-full max-w-md border border-white/30 text-center">
+    <div className="flex items-center justify-center min-h-screen  bg-gradient-to-br from-blue-400 via-blue-300 to-blue-500 p-4">
+      <div className="backdrop-blur-lg bg-white/20 mt-40 shadow-lg rounded-2xl p-6 w-full max-w-md border border-white/30 text-center">
         <h1 className="text-xl font-semibold text-white mb-4">AR/VR, APP & WEB DEV CHAPTER RECRUITMENT 2.0</h1>
         <iframe 
           src="https://ubiquitous-elderberry-49b.notion.site/ebd/1c3c5c15897080429a6ad7c9f52fa4f4" 
